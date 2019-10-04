3ee81675f32d6dc61e85d7bd5da181f30ae1bc78
Adryan Yudhatrisna <adryanyudhatrisna@adryans-mbp.attlocal.net>
2019-10-03 21:28:20 -0400

    Broken our homepage into smaller components, and added some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.components';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Homepage />
-      <h1>Hello world</h1>
     </div>
   );
 }

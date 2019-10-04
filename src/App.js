92a62c4edd2bf46e5935885ce2a775f069a50906
Adryan Yudhatrisna <adryanyudhatrisna@adryans-mbp.attlocal.net>
2019-10-03 21:55:55 -0400

    Added Switch, route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.components';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }

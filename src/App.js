7765200acf310b6bd0ebd89903464bd538bee4cb
adryanyudhatrisna <ayudhatrisna1989@gmail.com>
2019-10-24 22:00:03 -0400

    Added Sign-in components and forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Homepage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.components';
 import Header from './components/Header/header.components';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.components';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop/hats' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.components.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/Sign-in/sign-in.components';
+import './sign-in-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;

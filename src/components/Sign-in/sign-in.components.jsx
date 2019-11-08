f05c00ec0cb3dd67174160df3b95e1aec68b4e5a
adryanyudhatrisna <ayudhatrisna1989@gmail.com>
2019-11-08 14:22:17 -0500

    Implementing forms+components and style our sign in page

--- src/components/Sign-in/sign-in.components.jsx
+++ src/components/Sign-in/sign-in.components.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import FormInput from '../Form-input/form-input.components';
+import CustomButton from '../Custom-button/custom-button.components';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -29,24 +31,23 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='email'
             value={this.state.email}
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            label='password'
+            handleChange={this.handleChange}
             required
           />
-          <label>Password</label>
-
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'>Sign in</CustomButton>
         </form>
       </div>
     );
